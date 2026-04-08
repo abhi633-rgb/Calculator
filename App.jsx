b805e2b97c271eff04b5d677ab221f7da4d3e16b
Abhi <142966328+abhi633-rgb@users.noreply.github.com>
2026-04-08 11:42:37 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,21 +1,7 @@
-import { useState } from "react";
+import Calculator from "./components/Calculator";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div style={styles.container}>
-      <h1>🚀 React App</h1>
-
-      <p>Count: {count}</p>
-
-      <div style={styles.buttons}>
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-        <button onClick={() => setCount(count - 1)}>Decrement</button>
-        <button onClick={() => setCount(0)}>Reset</button>
-      </div>
-    </div>
-  );
+  return <Calculator />;
 }
 
 export default App;
